a3609029cc832771aca7a6d245f22e535d5d4221
KP <djkormo@gmail.com>
2020-07-04 22:55:22 +0200

    initial commit

---
+++ install/aks-network-policy-calico-install.bash
@@ -0,0 +1,214 @@
+#!/bin/bash
+
+# based on https://docs.microsoft.com/en-us/azure/container-instances/container-instances-using-azure-container-registry
+
+
+# -o create ,delete ,status. shutdown
+# -n aks-name
+# -g aks-rg
+# set your name and resource group
+
+# aks-network-policy-calico-install.bash -n aks-security2020 -g rg-aks -l northeurope, -o create
+
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+
+display_usage() { 
+	echo "Example of usage:" 
+	echo -e "bash $me -n aks-security2020 -g rg-aks -l northeurope -o create" 
+	echo -e "bash $me -n aks-security2020 -g rg-aks -l northeurope -o stop" 
+	echo -e "bash $me -n aks-security2020 -g rg-aks -l northeurope -o start" 
+	echo -e "bash $me -n aks-security2020 -g rg-aks -l northeurope -o status" 
+	echo -e "bash $me -n aks-security2020 -g rg-aks -l northeurope -o delete" 
+	} 
+
+while getopts n:g:o:l: option
+do
+case "${option}"
+in
+n) AKS_NAME=${OPTARG};;
+g) AKS_RG=${OPTARG};;
+o) AKS_OPERATION=${OPTARG};;
+l) AKS_LOCATION=${OPTARG};;
+esac
+done
+
+
+if [ -z "$AKS_OPERATION" ]
+then
+      echo "\$AKS_OPERATION is empty"
+	  display_usage
+	  exit 1
+else
+      echo "\$AKS_OPERATION is NOT empty"
+fi
+
+if [ -z "$AKS_NAME" ]
+then
+      echo "\$AKS_NAME is empty"
+	  display_usage
+	  exit 
+else
+      echo "\$AKS_NAME is NOT empty"
+fi
+
+if [ -z "$AKS_RG" ]
+then
+      echo "\$AKS_RG is empty"
+	  display_usage
+	  exit 1
+else
+      echo "\$AKS_RG is NOT empty"
+fi
+
+if [ -z "$AKS_LOCATION" ]
+then
+      echo "\$AKS_LOCATION is empty"
+	  display_usage
+	  exit 1
+else
+      echo "\$AKS_LOCATION is NOT empty"
+fi
+
+az aks get-versions -l $LOCATION #--query 'orchestrators[-1].orchestratorVersion' -o tsv
+
+AKS_VERSION=$(az aks get-versions -l $LOCATION --query 'orchestrators[-1].orchestratorVersion' -o tsv)
+
+AKS_NODES=2
+AKS_VM_SIZE=Standard_B2s
+
+echo "AKS_NODES: $AKS_NODES"
+echo "AKS_VERSION: $AKS_VERSION"
+echo "AKS_VM_SIZE: $AKS_VM_SIZE"
+
+if [ "$AKS_OPERATION" = "create" ] ;
+then
+
+    echo "Creating AKS cluster...";
+
+    # Install the aks-preview extension
+    az extension add --name aks-preview
+
+    # Update the extension to make sure you have the latest version installed
+    az extension update --name aks-preview
+
+    az feature register --name PodSecurityPolicyPreview --namespace Microsoft.ContainerService
+
+    az feature list -o table --query "[?contains(name, 'Microsoft.ContainerService/PodSecurityPolicyPreview')].{Name:name,State:properties.state}"
+    # wait until you have State = Registered
+
+    az provider register --namespace Microsoft.ContainerService
+
+    # Create a resource group
+    az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+
+    # Create a virtual network and subnet
+    az network vnet create \
+        --resource-group $RESOURCE_GROUP_NAME \
+        --name myVnet \
+        --address-prefixes 10.0.0.0/8 \
+        --subnet-name myAKSSubnet \
+        --subnet-prefix 10.240.0.0/16
+
+
+
+    # Create a service principal and read in the application ID
+    SP=$(az ad sp create-for-rbac --output json)
+    SP_ID=$(echo $SP | jq -r .appId)
+    SP_PASSWORD=$(echo $SP | jq -r .password)
+
+    # Wait 15 seconds to make sure that service principal has propagated
+    echo "Waiting for service principal to propagate..."
+    sleep 15
+
+    # Get the virtual network resource ID
+    VNET_ID=$(az network vnet show --resource-group $RESOURCE_GROUP_NAME --name myVnet --query id -o tsv)
+
+    # Assign the service principal Contributor permissions to the virtual network resource
+    az role assignment create --assignee $SP_ID --scope $VNET_ID --role Contributor
+
+    # Get the virtual network subnet resource ID
+    SUBNET_ID=$(az network vnet subnet show --resource-group $RESOURCE_GROUP_NAME --vnet-name myVnet --name myAKSSubnet --query id -o tsv)
+
+    # Create the AKS cluster and specify the virtual network and service principal information
+    # Enable network policy by using the `--network-policy` parameter
+    az aks create \
+        --resource-group $RESOURCE_GROUP_NAME \
+        --name $CLUSTER_NAME \
+        --vm-set-type AvailabilitySet \
+        --enable-addons monitoring \
+        --kubernetes-version $AKS_VERSION \
+        --node-vm-size $AKS_VM_SIZE \
+        --node-count $AKS_NODES \
+        --generate-ssh-keys \
+        --network-plugin azure \
+        --service-cidr 10.0.0.0/16 \
+        --dns-service-ip 10.0.0.10 \
+        --docker-bridge-address 172.17.0.1/16 \
+        --vnet-subnet-id $SUBNET_ID \
+        --service-principal $SP_ID \
+        --client-secret $SP_PASSWORD \
+        --network-policy calico
+
+
+    az aks update \
+        --resource-group $RESOURCE_GROUP_NAME \
+        --name $CLUSTER_NAME \
+        --enable-pod-security-policy
+
+    az aks update \
+        --resource-group $RESOURCE_GROUP_NAME \
+        --name $CLUSTER_NAME \
+        --disable-pod-security-policy
+
+
+fi # of create
+
+
+
+if [ "$AKS_OPERATION" = "start" ] ;
+then
+echo "starting VMs...";
+  # get the resource group for VMs
+  
+  RG_VM_POOL=$(az aks show -g $AKS_RG -n $AKS_NAME --query nodeResourceGroup -o tsv)
+  echo "RG_VM_POOL: $RG_VM_POOL"
+  
+  az vm list -d -g $RG_VM_POOL  | grep powerState 
+  az vm start --ids $(az vm list -g $RG_VM_POOL --query "[].id" -o tsv) --no-wait
+fi
+ 
+if [ "$AKS_OPERATION" = "stop" ] ;
+then
+echo "stopping VMs...";
+  # get the resource group for VMs
+  RG_VM_POOL=$(az aks show -g $AKS_RG -n $AKS_NAME --query nodeResourceGroup -o tsv)
+
+  echo "RG_VM_POOL: $RG_VM_POOL"
+
+  az vm list -d -g $RG_VM_POOL  | grep powerState
+
+  az vm deallocate --ids $(az vm list -g $RG_VM_POOL --query "[].id" -o tsv) --no-wait
+fi
+
+
+if [ "$AKS_OPERATION" = "status" ] ;
+then
+  echo "AKS cluster status"
+  az aks show --name $AKS_NAME --resource-group $AKS_RG
+  
+  # get the resource group for VMs
+  RG_VM_POOL=$(az aks show -g $AKS_RG -n $AKS_NAME --query nodeResourceGroup -o tsv)
+  echo "RG_VM_POOL: $RG_VM_POOL"
+  
+  az vm list -d -g $RG_VM_POOL  | grep powerState 
+  
+fi 
+
+
+if [ "$AKS_OPERATION" = "delete" ] ;
+then
+  echo "AKS cluster deleting ";
+  az aks delete --name $AKS_NAME --resource-group $AKS_RG
+  
+fi 
+
